173e8b70d66cd17c1cd9c9e75054263baabed283
m4punk <m4punk@gmail.com>
2014-07-04 15:29:16 -0400

    Finished layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,67 +2,41 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
-    end
+    it { should have_content('Sample App') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title(' | Home') }
 
-    it "should have the right title" do
-    	visit 'static_pages/home'
-    	expect(page).to have_title("Rails Tutorial ")
-    end
-
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title(' | Home')
-    end
   end
 
   describe "Help Page" do
+    before { visit help_path }
 
-    it "should have the content 'Help Me!'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help Me!')
-    end
+    it { should have_content('Help Me!') }
+    #it { should have_title(full_title("Help Me!")) }
+    it { should have_title("Help Me!") }
 
-        it "should have the right title" do
-    	visit 'static_pages/help'
-    	expect(page).to have_title("Help Me!")
-    end
   end
 
   describe "About Us" do
+    before { visit about_path }
 
-    it "should have the content 'About Us'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Us')
-    end
+    it { should have_content('About Us') }
+    #it { should have_title(full_title("About Us")) }
+    it { should have_title("About Us") }
 
-    it "should have the right title" do
-    	visit '/static_pages/about'
-    	expect(page).to have_title("About Us")
-    end
   end
 
   describe "Contact" do
+    before { visit contact_path }
 
-    it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
-      expect(page).to have_content('Contact Me')
-    end
+    it { should have_content('Contact') }
+    it { should have_title("Contact") }
+    #it { should have_title(full_title("Contact")) }
 
-    it "should have the right title" do
-    	visit '/static_pages/contact'
-    	expect(page).to have_title('Contact Me')
-    end
-
-    it "should have a Email Me href" do
-    	visit '/static_pages/contact'
-      page.find_link('Email Me')
-    	page.find_link('Home')
-
-    end
   end
 end
